b32e17710200f1072128f178b15905801c5f6379
dashkendall <131983174+dashkendall@users.noreply.github.com>
2025-06-10 00:42:52 +0000

    feat(generator): Added random generator for getting out of plans

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+document.getElementById("excuse").innerHTML = generateExcuse();
+
+
   //write your code here
-  console.log("Hello Rigo from the console!");
+    function generateExcuse() {
+
+      let chore = ['Walk', 'Wash', 'Prepare', 'Tuck in', 'Feed'];
+      let who = ['my cat', 'my dog', 'my grandma', 'the neighbor', 'a piece of string'];
+      let when = ['before', 'after', 'during', 'in the middle of'];
+      let task =['work', 'school', 'church', 'grocery shopping', 'yard work'];
+    
+        let randomChore = chore[Math.floor(Math.random() * chore.length)];
+        let randomWho = who[Math.floor(Math.random() * who.length)];
+        let randomWhen = when[Math.floor(Math.random() * when.length)];
+        let randomTask = task[Math.floor(Math.random() * task.length)];
+
+      return `${randomChore} ${randomWho} ${randomWhen} ${randomTask}`;
+      
+    }
+
+  let excuse = generateExcuse();
+  console.log(excuse);
 };
